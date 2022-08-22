691b64b9247c3bc59260054ba7cf88de809b1b75
Rahul Raj <rahulraj@tuna.group>
2022-08-22 11:25:36 +0530

    second commit

--- script.js
+++ script.js
@@ -3,58 +3,10 @@ const inputFile = document.querySelector('#file');
 const imgArea = document.querySelector('.img-area');
 const fileRemove = document.getElementById("remove-image");
 selectImage.addEventListener('click', function () {
-	inputFile.click();
+inputFile.click();
 
 	// img.style.visibility = (visible ? 'visible' : 'hidden');
 })
-
-//dragbox
-// dragElement(document.getElementById("mydiv"));
-
-// function dragElement(elmnt) {
-//   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-//   if (document.getElementById(elmnt.id + "header")) {
-//     /* if present, the header is where you move the DIV from:*/
-//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//   } else {
-//     /* otherwise, move the DIV from anywhere inside the DIV:*/
-//     elmnt.onmousedown = dragMouseDown;
-//   }
-
-//   function dragMouseDown(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // get the mouse cursor position at startup:
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     document.onmouseup = closeDragElement;
-//     // call a function whenever the cursor moves:
-//     document.onmousemove = elementDrag;
-//   }
-
-//   function elementDrag(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // calculate the new cursor position:
-//     pos1 = pos3 - e.clientX;
-//     pos2 = pos4 - e.clientY;
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     // set the element's new position:
-//     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-//     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-//   }
-
-//   function closeDragElement() {
-//     /* stop moving when mouse button is released:*/
-//     document.onmouseup = null;
-//     document.onmousemove = null;
-//   }
-// }
-
-//end here
-
-
 
 
 var testimage = [];
@@ -73,8 +25,6 @@ inputFile.addEventListener('change', function () {
 			imgArea.appendChild(img);
 			imgArea.classList.add('active');
 			imgArea.dataset.img = image.name;
-			// updateCanvas(img);
-			//  used to hide class
 			const box = document.getElementsByClassName('filebox')[0];
 			box.style.visibility = 'hidden';
 		}
@@ -83,43 +33,46 @@ inputFile.addEventListener('change', function () {
 });
 
 function newfunction(arr){
-	var panoramasArray = arr;
-	var panoramaNumber = Math.floor(Math.random()*panoramasArray.length);
-	renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-   
-    	// creating a new scene
-			var scene = new THREE.Scene();
+		var panoramasArray = arr;
+		var panoramaNumber = Math.floor(Math.random()*panoramasArray.length);
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.setPixelRatio( window.devicePixelRatio );
+		document.body.appendChild(renderer.domElement);
+
+		// creating a new scene
+		var scene = new THREE.Scene();
+
+		// adding a camera
+		var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000); camera.position.z = 1;
+		// camera.target = new THREE.Vector3(0, 0, 0);
+
+		// creation of a big sphere geometry
+		var sphere = new THREE.SphereGeometry(100, 100, 40);
+		sphere.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
+
+		// creation of the sphere material
+		var sphereMaterial = new THREE.MeshBasicMaterial();
+		sphereMaterial.map = THREE.ImageUtils.loadTexture(panoramasArray[panoramaNumber])
+
+		// geometry + material = mesh (actual object)
+		var sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
+		scene.add(sphereMesh);
+
+		const controls = new THREE.OrbitControls(camera, renderer.domElement);
+		// controls.maxPolarAngle = Math.PI/2.1;  
+
+		render();
 			
-			// adding a camera
-			var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000); camera.position.z = 1;
-			// camera.target = new THREE.Vector3(0, 0, 0);
-
-			// creation of a big sphere geometry
-			var sphere = new THREE.SphereGeometry(100, 100, 40);
-			sphere.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
-
-			// creation of the sphere material
-			var sphereMaterial = new THREE.MeshBasicMaterial();
-			sphereMaterial.map = THREE.ImageUtils.loadTexture(panoramasArray[panoramaNumber])
-
-			// geometry + material = mesh (actual object)
-			var sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
-			scene.add(sphereMesh);
-
-            const controls = new THREE.OrbitControls(camera, renderer.domElement);
-		    // controls.maxPolarAngle = Math.PI/2.1;  
-
-            render();
-               
-            function render(){
-				requestAnimationFrame(render);
-				// calling again render function
-				// controls.update();
-				renderer.render(scene, camera);
-			}
+		function render(){
+			requestAnimationFrame(render);
+			// calling again render function
+			// controls.update();
+			renderer.render(scene, camera);
+		}
 	//console.log(panoramaNumber);
 }
+
+
 	
 	
